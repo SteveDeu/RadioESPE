75f05c31c02656b4b1e69263503c3c5847da7546
Steven Aizaga <stevenaizaga@gmail.com>
2017-01-05 19:14:41 -0500

    Version Windows

---
+++ Versiones/Windows Phone/Version 1/APA_RadioESPE/AudioPlayer.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using Microsoft.Phone.BackgroundAudio;
+using System.Collections.Generic;
+
+namespace APA_RadioESPE
+{
+    public class AudioPlayer : AudioPlayerAgent
+    {
+        /// <remarks>
+        /// del agente en segundo plano pueden compartir el mismo proceso.
+        /// Los campos estáticos pueden utilizarse para compartir el estado entre las instancias de AudioPlayer
+        /// o para comunicarse con el agente de transmisión por secuencias de audio.
+        /// </remarks>
+
+        static int currentTrackNumber = 0;
+
+        static AudioPlayer()
+        {
+            // Suscribirse al controlador de excepciones administradas
+            Deployment.Current.Dispatcher.BeginInvoke(delegate
+            {
+                Application.Current.UnhandledException += UnhandledException;
+            });
+        }
+
+        /// Código para ejecutar en excepciones no controladas
+        private static void UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
+        {
+            if (Debugger.IsAttached)
+            {
+                // Se ha producido una excepción no controlada; interrumpir el depurador
+                Debugger.Break();
+            }
+        }
+
+        /// <summary>
+        /// Se llama cuando cambia el estado de reproducción, excepto en el caso del estado Error (consulte OnError)
+        /// </summary>
+        /// <param name="player">El objeto BackgroundAudioPlayer</param>
+        /// <param name="track">Pista que se estaba reproduciendo en el momento que cambió el estado de reproducción</param>
+        /// <param name="playState">Nuevo estado de reproducción del reproductor</param>
+        /// <remarks>
+        /// No se pueden cancelar los cambios del estado de reproducción. Estos cambios se desencadenan aunque sea la propia aplicación
+        /// la que causa el cambio de estado, suponiendo que la aplicación participe en la devolución de llamada.
+        ///
+        /// Eventos del estado de reproducción notables:
+        /// (a) TrackEnded: se invoca cuando el reproductor no tiene pista actual. El agente se puede establecer en la siguiente pista.
+        /// (b) TrackReady: se ha establecido una pista de audio que ahora está preparada para reproducirse.
+        ///
+        /// Llamar a NotifyComplete() solo una vez, después de que se haya completado la solicitud del agente, incluidas las devoluciones de llamada asincrónicas.
+        /// </remarks>
+        protected override void OnPlayStateChanged(BackgroundAudioPlayer player, AudioTrack track, PlayState playState)
+        {
+            switch (playState)
+            {
+                case PlayState.TrackEnded:
+                    player.Track = GetPreviousTrack();
+                    break;
+                case PlayState.TrackReady:
+                    player.Play();
+                    break;
+                case PlayState.Shutdown:
+                    // TODO: Controlar el estado de apagado aquí (por ejemplo, estado de almacenamiento)
+                    break;
+                case PlayState.Unknown:
+                    break;
+                case PlayState.Stopped:
+                    break;
+                case PlayState.Paused:
+                    break;
+                case PlayState.Playing:
+                    break;
+                case PlayState.BufferingStarted:
+                    break;
+                case PlayState.BufferingStopped:
+                    break;
+                case PlayState.Rewinding:
+                    break;
+                case PlayState.FastForwarding:
+                    break;
+            }
+
+            NotifyComplete();
+        }
+
+        /// <summary>
+        /// Se llama cuando el usuario solicita una acción mediante la interfaz de usuario proporcionada por la aplicación o el sistema
+        /// </summary>
+        /// <param name="player">El objeto BackgroundAudioPlayer</param>
+        /// <param name="track">Pista que se estaba reproduciendo en el momento de la acción del usuario</param>
+        /// <param name="action">Acción solicitada por el usuario</param>
+        /// <param name="param">Datos asociados a la acción solicitada.
+        /// En la versión actual este parámetro sólo se usa con la acción Seek
+        /// para indicar la posición solicitada de una pista de audio</param>
+        /// <remarks>
+        /// Las acciones del usuario no realizan cambios automáticamente en el estado del sistema; el agente es responsable
+        /// para llevar a cabo las acciones del usuario si son compatibles.
+        ///
+        /// Llamar a NotifyComplete() solo una vez, después de que se haya completado la solicitud del agente, incluidas las devoluciones de llamada asincrónicas.
+        /// </remarks>
+        protected override void OnUserAction(BackgroundAudioPlayer player, AudioTrack track, UserAction action, object param)
+        {
+            switch (action)
+            {
+                case UserAction.Play:
+                    PlayTrack(player);
+                    break;
+                case UserAction.Stop:
+                    player.Stop();
+                    break;
+                case UserAction.Pause:
+                    player.Pause();
+                    break;
+                case UserAction.FastForward:
+                    player.FastForward();
+                    break;
+                case UserAction.Rewind:
+                    player.Rewind();
+                    break;
+                case UserAction.Seek:
+                    player.Position = (TimeSpan)param;
+                    break;
+                case UserAction.SkipNext:
+                    //player.Track = GetNextTrack();
+                    break;
+                case UserAction.SkipPrevious:
+                    /*AudioTrack previousTrack = GetPreviousTrack();
+                    if (previousTrack != null)
+                    {
+                        player.Track = previousTrack;
+                    }*/
+                    break;
+            }
+
+            NotifyComplete();
+        }
+
+        /// <summary>
+        /// Implementa la lógica para obtener la siguiente instancia de AudioTrack.
+        /// En una lista de reproducción, el origen puede ser un archivo, una solicitud web, etc.
+        /// </summary>
+        /// <remarks>
+        /// El URI de AudioTrack determina el origen, que puede ser:
+        /// (a) Archivo de almacenamiento aislado (URI relativo, representa la ruta de acceso en el almacenamiento aislado)
+        /// (b) Dirección URL HTTP (URI absoluto)
+        /// (c) MediaStreamSource (null)
+        /// </remarks>
+        /// <returns>una instancia de AudioTrack, o null si se ha completado la reproducción</returns>
+        private AudioTrack GetNextTrack()
+        {
+            // TODO: agregar lógica para obtener la siguiente pista de audio
+
+            AudioTrack track = null;
+
+            // especificar la pista
+
+            return track;
+        }
+
+        /// <summary>
+        /// Implementa la lógica para obtener la instancia anterior de AudioTrack.
+        /// </summary>
+        /// <remarks>
+        /// El URI de AudioTrack determina el origen, que puede ser:
+        /// (a) Archivo de almacenamiento aislado (URI relativo, representa la ruta de acceso en el almacenamiento aislado)
+        /// (b) Dirección URL HTTP (URI absoluto)
+        /// (c) MediaStreamSource (null)
+        /// </remarks>
+        /// <returns>una instancia de AudioTrack, o null si la pista anterior no se permite</returns>
+        private AudioTrack GetPreviousTrack()
+        {
+            // TODO: agregar lógica para obtener la pista de audio anterior
+
+            AudioTrack track = null;
+
+            // especificar la pista
+
+            return track;
+        }
+
+        /// <summary>
+        /// Se llama siempre que se produce un error con el estado de reproducción, por ejemplo, si AudioTrack no se descarga correctamente
+        /// </summary>
+        /// <param name="player">El objeto BackgroundAudioPlayer</param>
+        /// <param name="track">Pista donde se produjo el error</param>
+        /// <param name="error">Error producido</param>
+        /// <param name="isFatal">Si es True, la reproducción no podrá continuar y se detendrá la reproducción de la pista</param>
+        /// <remarks>
+        /// No existen garantías de que se llame a este método en todos los casos. Por ejemplo, si las instancias de AudioPlayer
+        /// tiene una excepción no controlada, no se incluirá en ninguna devolución de llamada para que administre sus propios errores.
+        /// </remarks>
+        protected override void OnError(BackgroundAudioPlayer player, AudioTrack track, Exception error, bool isFatal)
+        {
+            if (isFatal)
+            {
+                Abort();
+            }
+            else
+            {
+                NotifyComplete();
+            }
+
+        }
+
+        /// <summary>
+        /// Se llama cuando se va a cancelar la solicitud del agente
+        /// </summary>
+        /// <remarks>
+        /// Una vez cancelada la solicitud, el agente obtiene 5 segundos para finalizar el trabajo,
+        /// mediante una llamada a NotifyComplete()/Abort().
+        /// </remarks>
+        protected override void OnCancel()
+        { }
+
+        private static List<AudioTrack> _playList = new List<AudioTrack>
+        {
+            new AudioTrack(new Uri("http://radio.espe.edu.ec/radioeskpe", UriKind.Absolute),
+                "Radio ESPE",
+                "ESKPE",
+                "Windows Phone-Radio ESPE",
+                null)
+        };
+
+        private void PlayTrack(BackgroundAudioPlayer player)
+        {
+            if((player.Track == null) || (player.Track.Title != _playList[currentTrackNumber].Title))
+            {
+                player.Track = _playList[currentTrackNumber];
+            }
+
+            if ((player.Track != null) && (player.PlayerState != PlayState.Playing))
+            {
+                player.Play();
+            }
+        }
+    }
+}
---
+++ Versiones/Windows Phone/Version 1/Radio ESPE/MainPage.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Radio_ESPE.Resources;
+using Microsoft.Phone.BackgroundAudio;
+using Microsoft.Phone.Tasks;
+
+namespace Radio_ESPE
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BackgroundAudioPlayer.Instance.PlayStateChanged += new EventHandler(Instance_PlayStateChanged);
+        }
+
+        #region Button Click Event Handlers
+        private void playButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (PlayState.Playing == BackgroundAudioPlayer.Instance.PlayerState)
+            {
+                BackgroundAudioPlayer.Instance.Pause();
+            }
+            else
+            {
+                BackgroundAudioPlayer.Instance.Play();
+            }
+        }
+
+        private void btnFacebook_Click(object sender, EventArgs e)
+        {
+            WebBrowserTask webBrowserTask = new WebBrowserTask();
+            webBrowserTask.Uri = new Uri("https://m.facebook.com/radioespe/");
+            webBrowserTask.Show();
+        }
+
+        private void btnTwitter_Click(object sender, EventArgs e)
+        {
+            WebBrowserTask webBrowserTask = new WebBrowserTask();
+            webBrowserTask.Uri = new Uri("https://mobile.twitter.com/radioespe");
+            webBrowserTask.Show();
+        }
+
+        private void btnInstagram_Click(object sender, EventArgs e)
+        {
+            WebBrowserTask webBrowserTask = new WebBrowserTask();
+            webBrowserTask.Uri = new Uri("https://www.instagram.com");
+            webBrowserTask.Show();
+        }
+
+        private void btnYoutube_Click(object sender, EventArgs e)
+        {
+            WebBrowserTask webBrowserTask = new WebBrowserTask();
+            webBrowserTask.Uri = new Uri("https://m.youtube.com/channel/UCrxUvo-lwZTPeqrNX6cNxyQ");
+            webBrowserTask.Show();
+        }
+
+        private void btnTemporizador_Click(object sender, EventArgs e)
+        {
+            //NavigationService.Navigate(new Uri("/SecondPage.xaml", UriKind.Relative));
+            WebBrowserTask webBrowserTask = new WebBrowserTask();
+            //webBrowserTask.Uri = new Uri("http://www.google.com");
+            webBrowserTask.Uri = new Uri("https://lh3.googleusercontent.com/-RKJEe4U7Ee0/VgNFN0lk3dI/AAAAAAAAPHY/RYyUW7WpaN8/w500-h500/5-chicas-hermosas-de-Gainax-que-debes-conocer-16-Animemx.gif");
+            webBrowserTask.Show();
+        }
+        #endregion Button Click Event Handlers
+
+        void Instance_PlayStateChanged(object sender, EventArgs e)
+        {
+            switch (BackgroundAudioPlayer.Instance.PlayerState)
+            {
+                case PlayState.Playing:
+                    playButton.Content = "pause";
+                    break;
+                case PlayState.Paused:
+                case PlayState.Stopped:
+                    playButton.Content = "play";
+                    break;
+            }
+
+            if (null != BackgroundAudioPlayer.Instance.Track)
+            {
+                txtCurrentTrack.Text = BackgroundAudioPlayer.Instance.Track.Title + " by " + BackgroundAudioPlayer.Instance.Track.Artist;
+            }
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (PlayState.Playing == BackgroundAudioPlayer.Instance.PlayerState)
+            {
+                playButton.Content = "pause";
+                txtCurrentTrack.Text = BackgroundAudioPlayer.Instance.Track.Title + " by " + BackgroundAudioPlayer.Instance.Track.Artist;
+            }
+            else
+            {
+                playButton.Content = "play";
+                txtCurrentTrack.Text = "";
+            }
+        }
+
+        private void btnPeticiones_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtPeticion.Text.Length <= 0)
+            {
+                MessageBox.Show("Ingresa datos °<° ");
+                txtPeticion.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Gracias por tu sugerencia! :v");
+                txtPeticion.Text = "";
+            }
+        }
+    }
+}
